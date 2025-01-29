9d3a867d3e3e9e2161860c2f3b4ec631b453b4fe
john.ramar <john.ramar@r2stech.co.uk>
2025-01-29 18:37:48 +0530

    lint fix

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -242,7 +242,7 @@ function LoginForm() {
               </button>
 
               <p className="text-center text-sm text-primary-600 mt-4">
-                Don't have an account?{' '}
+                Don&apos;t have an account?{''}
                 <Link 
                   href="/signup" 
                   className="font-medium text-accent hover:text-accent-dark 
